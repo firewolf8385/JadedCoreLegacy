315ec83cbc09307702ea1c5d76a8601323c3a13e
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2023-12-28 01:19:34 -0500

    Adds MathUtils#format as a new utility method

--- src/main/java/net/jadedmc/jadedutils/MathUtils.java
+++ src/main/java/net/jadedmc/jadedutils/MathUtils.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of JadedCoreLegacy, licensed under the MIT License.
+ * This file is part of JadedUtils, licensed under the MIT License.
  *
  *  Copyright (c) JadedMC
  *  Copyright (c) contributors
@@ -29,11 +29,27 @@ import org.bukkit.util.Vector;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 /**
  * A group of math-related utilities.
  */
 public class MathUtils {
+
+    /**
+     * A map of various suffixes when converting numbers to abbreviated forms.
+     */
+    private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+    static {
+        suffixes.put(1_000L, "k");
+        suffixes.put(1_000_000L, "M");
+        suffixes.put(1_000_000_000L, "G");
+        suffixes.put(1_000_000_000_000L, "T");
+        suffixes.put(1_000_000_000_000_000L, "P");
+        suffixes.put(1_000_000_000_000_000_000L, "E");
+    }
 
     /**
      * Calculate the distance between two Locations.
@@ -43,6 +59,25 @@ public class MathUtils {
      */
     public static double distance(final Location a, final Location b) {
         return Math.sqrt(square(a.getX() - b.getX()) + square(a.getY() - b.getY()) + square(a.getZ() - b.getZ()));
+    }
+
+    /**
+     * Turn a number into it's abbreviated spring form.
+     * @param value Number to be converted.
+     * @return Abbreviated number.
+     */
+    public static String format(long value) {
+        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+        if (value < 0) return "-" + format(-value);
+        if (value < 1000) return Long.toString(value);
+
+        Map.Entry<Long, String> e = suffixes.floorEntry(value);
+        Long divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        long truncated = value / (divideBy / 10);
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
     }
 
     /**
